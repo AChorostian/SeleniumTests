b5c3c1f48d3353bc618aa0e71f6bf0982a3fbf3e
Andrzej Chorostian <andrzejchorostian@outlook.com>
2019-05-19 13:43:27 +0200

    Init junit5 test project

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SampleTest
+{
+
+    @Test
+    void sample1Test()
+    {
+        assertTrue(true);
+    }
+
+    @Test
+    void sample2Test()
+    {
+        assertTrue(true);
+    }
+
+}
